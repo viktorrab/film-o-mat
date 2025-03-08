aed628baf0db7130971474c88494c3390d23df7e
viktorrab <viktorrabinovitsch@web.de>
2025-03-08 21:52:19 +0100

    Fix missing button component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 
 const questions = [
   { id: 1, text: "Welches Genre bevorzugst du?", options: ["Action", "Drama", "Sci-Fi", "Horror"] },
