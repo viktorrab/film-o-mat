e2a2809a3a89a9a20c718a143b5ac6df97708f7d
viktorrab <viktorrabinovitsch@web.de>
2025-03-08 21:46:09 +0100

    Added missing button component

---
+++ pages/components/ui/button.js
@@ -0,0 +1,11 @@
+export function Button({ children, onClick }) {
+    return (
+      <button 
+        onClick={onClick} 
+        style={{ padding: "10px", background: "blue", color: "white", borderRadius: "5px", border: "none", cursor: "pointer" }}
+      >
+        {children}
+      </button>
+    );
+  }
+  
